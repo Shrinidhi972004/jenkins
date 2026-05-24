44670181c56f9cea5d696c7a362a3e54a3578d15
Shrinidhi972004 <shrinidhiupadhyaya00@gmail.com>
2026-05-24 12:48:39 +0530

    jenkins agent pipeline trigger

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Hello from Jenkins Pipeline!"
+    return "Hello from Jenkins Pipeline from shrinidhi!"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
